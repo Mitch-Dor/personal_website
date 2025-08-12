3c9f7866796d355b526ea194e61bb16bda38b977
Mitchell_Dorward <mgd225@lehigh.edu>
2025-08-12 13:57:40 -0400

    Carousel now has the ability to expand images

--- src/components/sideComponents/js/ImageCarousel.jsx
+++ src/components/sideComponents/js/ImageCarousel.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+import { VscClose } from "react-icons/vsc";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import "../css/imageCarousel.css";
 
 export default function ImageCarousel() {
+  const [expandedImage, setExpandedImage] = useState(null);
   const images = [
     "/assets/carousel/breloom.gif",
     "/assets/carousel/loudred.gif",
@@ -21,14 +25,26 @@ export default function ImageCarousel() {
   };
 
   return (
-    <div className="carousel">
-        <Slider {...settings}>
-        {images.map((src, i) => (
-            <div key={i} className="carouselImageContainer">
-                <img src={src} alt={`Slide ${i + 1}`} style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+    <>
+      {expandedImage && (
+        <div className="screenCover" onClick={() => {setExpandedImage(null)}}>
+          <div className="imageContainer">
+            <div className="expandedImageClose" onClick={() => {setExpandedImage(null)}}>
+              <VscClose className="closeIcon" />
             </div>
-        ))}
-        </Slider>
-    </div>
+            <img src={expandedImage}></img>
+          </div>
+        </div>
+      )}
+      <div className="carousel">
+          <Slider {...settings}>
+          {images.map((src, i) => (
+              <div key={i} className="carouselImageContainer">
+                  <img src={src} onClick={() => {setExpandedImage(src)}} alt={`Slide ${i + 1}`} style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+              </div>
+          ))}
+          </Slider>
+      </div>
+    </>
   );
 }
